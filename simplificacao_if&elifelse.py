1c24f9ff91771c05ed8001b82aa38f3509022e11
nicollyemvocacao <nicollyvitoriagon@gmail.com>
2026-03-09 16:57:41 -0300

    update - Atividade com prof Ivan

---
+++ simplificacao_if&elifelse.py
@@ -0,0 +1,10 @@
+preco_frutas= {
+    'maçã':2.5,
+    'banana': 1.8,
+    'laranja': 3.0
+}
+
+
+fruta_desejada='maçã'
+resultado = preco_frutas.get(fruta_desejada, 'Fruta não encontrada')
+print(f'O preço da fruta {fruta_desejada} é R$ {resultado}')
---
+++ teste_idade.py
@@ -0,0 +1,20 @@
+'''
+
+
+'''
+
+print('Digite a idade do jovem: ')
+
+idade = int(input())
+
+if idade <= 18:
+    print('Jovem é menor de idade')
+
+elif idade <=65:
+    print('Você é Adulto')
+
+elif idade <=25:
+    print ('você é jovem adulto')
+
+elif idade <200:
+    print ('você é idoso')
